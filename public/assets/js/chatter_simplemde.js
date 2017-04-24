5918bd248d5b6e313ef598e1aa0eb3ef5b7eab98
yirideveloper <topyiri1113@gmail.com>
2017-04-24 09:17:13 -0700

    fix - forward to login route if guest

--- public/assets/js/chatter_simplemde.js
+++ public/assets/js/chatter_simplemde.js
@@ -1,3 +1,4 @@
+
 if (typeof simplemdeOptions == 'undefined') {
 	var simplemdeOptions = {
 		autofocus: true,
@@ -16,8 +17,7 @@ function newSimpleMde(element){
 $('document').ready(function(){
 
 	var simplemde = newSimpleMde(document.getElementById("simplemde"));
-	var simplemdeInDiscussionView = newSimpleMde(document.getElementById("simplemde_in_discussion_view"));
-
+	
 	$('.editor-toolbar .fa-columns').click(function(){
 		if(!$('body').hasClass('simplemde')){
 			$('body').addClass('simplemde');
@@ -34,10 +34,4 @@ $('document').ready(function(){
 
 	document.getElementById('new_discussion_loader').style.display = "none";
 	document.getElementById('chatter_form_editor').style.display = "block";
-
-    // check if user is in discussion view
-    if ($('#new_discussion_loader_in_discussion_view').length > 0) {
-        document.getElementById('new_discussion_loader_in_discussion_view').style.display = "none";
-        document.getElementById('chatter_form_editor_in_discussion_view').style.display = "block";
-    }
 });
